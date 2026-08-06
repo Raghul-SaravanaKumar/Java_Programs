76b8df6ad5282cfec45d0e0faaf79fb7a010373d
Raghul18 <rexizzz1815@gmail.com>
2026-08-06 09:39:57 +0530

    Java practice

---
+++ Anagrampractice.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagrampractice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine().toLowerCase();
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine().toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            System.out.println("The strings are not anagrams.");
+        } else {
+            char[] arr1 = str1.toCharArray();
+            char[] arr2 = str2.toCharArray();
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            if (Arrays.equals(arr1, arr2)) {
+                System.out.println("The strings are anagrams.");
+            } else {
+                System.out.println("The strings are not anagrams.");
+            }
+        }
+    }
+}
