ea502201790c87f2e1dff5ec7291ffa9ecb70fb3
Raghul18 <rexizzz1815@gmail.com>
2026-08-07 10:27:09 +0530

    Java programs

--- FirstRepeating_ch.java
+++ FirstRepeating_ch.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class FirstRepeating_ch {
-    
+    public static void main(String [] test){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            char ch2 = ch;
+            if (str.indexOf(ch) != str.lastIndexOf(ch)) {
+                System.out.println("The first repeating character is: " + ch);
+                return;
+            }
+            if(str.indexOf(ch) == str.lastIndexOf(ch) ){
+
+            }
+        }
+        System.out.println("No repeating character found.");
+    }
 }
---
+++ Secondreepating_ch.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Secondreepating_ch {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (str.indexOf(ch) != str.lastIndexOf(ch)
+                    && str.indexOf(ch) == i) {
+
+                count++;
+
+                if (count == 2) {
+                    System.out.println(ch);
+                    return;
+                }
+            }
+        }
+
+        System.out.println("No second repeating character");
+    }
+}
