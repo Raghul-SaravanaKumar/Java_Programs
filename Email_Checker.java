5453f4e873d3d4ee74ca7728ab78f78b5ed83650
Raghul18 <rexizzz1815@gmail.com>
2026-08-08 22:44:46 +0530

    Java Programs

---
+++ Email_Checker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Email_Checker {
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an email address: ");
+        String email = sc.nextLine();
+
+        if (email.contains("@")
+                && email.contains(".")
+                && email.indexOf("@") < email.lastIndexOf(".")) {
+
+            System.out.println("Valid Email");
+        } else {
+            System.out.println("Invalid Email");
+        }
+    }
+}
