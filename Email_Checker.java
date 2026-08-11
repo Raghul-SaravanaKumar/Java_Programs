c3f9b14d2776abc28aa4a2b32163f1bcd5803547
Raghul18 <rexizzz1815@gmail.com>
2026-08-11 22:16:39 +0530

    Java programs

--- Email_Checker.java
+++ Email_Checker.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Email_Checker {
     Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter an email address: ");
