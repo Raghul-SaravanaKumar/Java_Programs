fffe0fd8e36ec253ea591788f31d51b4d6152172
Raghul18 <rexizzz1815@gmail.com>
2026-08-11 22:19:35 +0530

    Java programs

--- Allrepeating_ch.java
+++ Allrepeating_ch.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Allrepeating_ch {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
