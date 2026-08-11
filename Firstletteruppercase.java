8c737373e9e456f38fcf9e520104c39e91d338b0
Raghul18 <rexizzz1815@gmail.com>
2026-08-11 22:19:07 +0530

    Java programs

--- Firstletteruppercase.java
+++ Firstletteruppercase.java
@@ -7,7 +7,6 @@ public class Firstletteruppercase {
         String str = sc.nextLine();
         String[] words = str.split(" ");
         StringBuilder result = new StringBuilder();
-
         for (String word : words) {
             if (word.length() > 0) {
                 result.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1)).append(" ");
