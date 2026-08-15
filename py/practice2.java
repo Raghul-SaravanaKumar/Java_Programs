bcb374e6e60951ba3559e1bdf0bc9aded113034b
Raghul18 <rexizzz1815@gmail.com>
2026-08-15 23:24:06 +0530

    Java program

--- py/practice2.java
+++ py/practice2.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
+
 public class practice2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
-        String [] result = str.split(" ");
+        String[] result = str.split(" ");
 
         if (result.length > 1) {
             for (String word : result) {
-                System.out.println(word);
+                System.out.print(word);
             }
         } else {
             System.out.println("There is no space in the sentence");
